db134c9bbda225d8618822b72e14a9fb3ad14647
Letri88 <youngbtoxic04@gmail.com>
2026-03-05 11:43:22 +0700

    Add project files.

---
+++ DACN_CNPM_QuanLyXayDung/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using DACN_CNPM_QuanLyXayDung.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DACN_CNPM_QuanLyXayDung.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
